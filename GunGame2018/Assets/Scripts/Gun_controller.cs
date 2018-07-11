012ce4fff592c875f94cc422e78d39ad96641535
PixelPerfect <felix.gustavsson90@gmail.com>
2018-07-11 18:13:54 +0200

    Larger Bullet Text

--- GunGame2018/Assets/Scripts/Gun_controller.cs
+++ GunGame2018/Assets/Scripts/Gun_controller.cs
@@ -43,7 +43,7 @@ public class Gun_controller : MonoBehaviour {
 
     private void writeBulletsOnUI()
     {
-        GameObject.Find("Bullets_count").GetComponent<Text>().text = "Bullets " + bullets;
+        GameObject.Find("Bullets_count").GetComponent<Text>().text = "Bullets :" + bullets;
     }
 
     public void reload()
