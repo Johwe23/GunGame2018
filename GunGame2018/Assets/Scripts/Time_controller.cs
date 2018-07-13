751f31847d1a9a7d82af779cc24b989735477e38
PixelPerfect <felix.gustavsson90@gmail.com>
2018-07-13 23:00:44 +0200

    Revert "Revert "Added Time and some minor changes""
    This reverts commit 33b65d418e46b8c8500fb3a56c7fc88413c457e1.

---
+++ GunGame2018/Assets/Scripts/Time_controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Time_controller : MonoBehaviour
+{
+    [SerializeField]
+    private Text uiText;
+
+    [SerializeField]
+    private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool stop = false;
+
+    private int displayTime;
+
+    void Start()
+    {
+        timer = mainTimer;
+    }
+
+    void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("F");
+        }
+        else if (timer <= 0.0f && !stop)
+        {
+            canCount = false;
+            uiText.text = "0";
+            timer = 0.0f;
+            stop = true;
+        }
+    }
+}
